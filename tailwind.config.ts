cf03c2015cba01e10eec6296e3b2e6f34b9c8c3c
gulshansinghAIT <gulshankumar_22224@aitpune.edu.in>
2025-01-11 22:12:21 +0530

    removed errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,31 @@
-import type { Config } from "tailwindcss";
+import { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [
-    function ({ addUtilities }) {
+    plugin(function ({ addUtilities }) {
       addUtilities({
-        ".scrollbar-none": {
-          "&::-webkit-scrollbar": { display: "none" },
-          "-ms-overflow-style": "none", // IE and Edge
-          "scrollbar-width": "none", // Firefox
+        '.scrollbar-none': {
+          '&::-webkit-scrollbar': { display: 'none' },
+          '-ms-overflow-style': 'none', // IE and Edge
+          'scrollbar-width': 'none', // Firefox
         },
       });
-    },
+    }),
   ],
-} satisfies Config;
+};
+
+export default config;
